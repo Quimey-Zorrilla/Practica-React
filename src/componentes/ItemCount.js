1a216c85550a8cac0cd5f96de6643ebc4f012608
Quimey-Zorrilla <zorrillaquimey@gmail.com>
2022-03-21 12:07:45 -0300

    Eventos-agregar contador

--- src/componentes/ItemCount.js
+++ src/componentes/ItemCount.js
@@ -1,32 +1,31 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
 
-const ItemCount = ({stock = 5, onAdd}) => {
-    const [contador, setContador] = useState(0);
+const ItemCount = ({stock,initial,onAdd}) => {
+    const [contador, setContador] = useState(initial)
+
+    const aumentar = (e) => {
+        if(contador < stock) {
+            setContador(contador + 1)
+        }
+    }
     
-    const agregar = () => {
-        setContador(Math.min(stock, contador + 1));
+    const disminuir = (e) => {
+        if(contador > initial) {
+            setContador(contador - 1)
+        }
     }
 
-    const restar = () => {
-        setContador(Math.max(0, contador - 1));
+    const confirmar = (e) => {
+        onAdd(contador)
     }
-
-    return (
-        <div className="contenedor">
-            <button className="btnAgregar" onClick={agregar}>+</button>
-            <span>
-                {contador}
-            </span>
-            <button className="btnRestar" onClick={restar}>-</button>
-            <button className="btnAgregar" onClick={onAdd}>Agregar al carrito</button>
+    return(
+        <div>
+            <p>Cantidad actual: {contador}</p>
+            <button onClick={aumentar}> + </button>
+            <button onClick={disminuir}> - </button>
+            <button onClick={confirmar}> Confirmar compra </button>
         </div>
-    )
-}
-
-ItemCount.propTypes = {
-    stock: PropTypes.number,
-    onAdd: PropTypes.func
+    );
 }
 
 export default ItemCount;
--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ItemCount from './ItemCount';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const ItemDetail = ({
     id,
@@ -7,15 +10,31 @@ export const ItemDetail = ({
     image,
     description,
     price,
+    
 }) => {
+    const [seleccionado, setSeleccionado] = useState(false)
+    const [agregar, setAgregar] = useState(false)
+
+    const onAdd = (unidadSeleccionada) => {
+        if(unidadSeleccionada !== undefined){
+            setSeleccionado(true)
+        }
+        setAgregar(true)
+    }
     return (
         <div className="detalles">
             <img src={image} alt={`${id}-${name}`} className="flex-col" />
-            <section>
-                <h1>{name}</h1>
-                <p>{description}</p>
-                <h2>${price}</h2>
-            </section>
+            <div>
+                <section>
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                    <h2>${price}</h2>
+                </section>
+                <section>
+                    <p>{seleccionado?"Compra realizada!":"No selecciono cantidad"}</p>
+                    {agregar?<Link to="/carrito/">Ir al carrito</Link> : <ItemCount initial={1} stock={5} onAdd={onAdd}/> }
+                </section>
+            </div>
         </div>
     );
 };
